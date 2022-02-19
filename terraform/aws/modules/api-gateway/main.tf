6b5a3942052c7e014925d54a99f6b80ff178b34d
Dimitrios Dentsas <d.dentsas@googlemail.com>
2022-02-19 23:40:24 +0100

    adjust nlb and alb target groups and listeners; add custom domain name mapping

--- terraform/aws/modules/api-gateway/main.tf
+++ terraform/aws/modules/api-gateway/main.tf
@@ -75,3 +75,15 @@ resource "aws_api_gateway_deployment" "main" {
     create_before_destroy = true
   }
 }
+
+resource "aws_api_gateway_stage" "main" {
+  deployment_id = aws_api_gateway_deployment.main.id
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  stage_name    = "${var.environment}-env"
+}
+
+resource "aws_api_gateway_base_path_mapping" "main" {
+  api_id      = aws_api_gateway_rest_api.main.id
+  stage_name  = "${var.environment}-env"
+  domain_name = aws_api_gateway_domain_name.main.domain_name
+}
