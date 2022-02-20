acb142dbce39b05f23a6b924706a2e8b1686ab02
Dimitrios Dentsas <d.dentsas@googlemail.com>
2022-02-20 22:26:44 +0100

    remove unneeded variable

--- terraform/aws/applications/ecr-repository/variable.tf
+++ terraform/aws/applications/ecr-repository/variable.tf
@@ -1,8 +1,3 @@
-variable "aws_ecr_repository_name" {
-  type = string
-  description = "Name of the ECR repository to be created"
-}
-
 variable "aws_region" {
   type = string
   description = "AWS region"
